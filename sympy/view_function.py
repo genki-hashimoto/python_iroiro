f1ef303b9ae613ce679d56ac583e9f1091e39920
Genki Hashimoto <knct.gen@gmail.com>
2015-06-05 16:14:56 +0900

    あとの変更は忘れたよ！！！

--- sympy/view_function.py
+++ sympy/view_function.py
@@ -6,7 +6,10 @@ import math
 x = sym.Symbol("x")
 e = sym.Symbol("e")
 
-f = 1 / (1 + sym.exp(-e * x))
+#f = 1 / (1 + sym.exp(-e * x))
+f = 3* sym.atan(x-1) + x / 4
+df = sym.diff(f)
 
 #matplotlibによるプロット機能もありますxlimだけでなく表題や軸の設定なども引数で設定できる
-sym.plotting.plot(f.subs([(e,1)]),(x,-10,10))
+#sym.plotting.plot(f.subs([(e,1)]),(x,-10,10))
+sym.plotting.plot(f,df,(x,0,4),color = "b")
